c34fb7acc797eccd3c5e45136ac3e20fb1fe43c2
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-16 09:54:01 -0400

    getting the different aspects of the weather saved to variables from the API

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,10 +10,36 @@ function getWeather(c) {
     })
     .then((response) => {
       console.log(response);
-      let temp = response.main.temp;
-      let humidity = response.main.humidity;
-      let windSpeed = response.wind.speed;
-      console.log(temp, humidity, windSpeed);
+      let lat = response.coord.lat;
+      let lon = response.coord.lon;
+
+      console.log(lon, lat);
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=a6d51def84a4c7a5cfb3387c9243b441`
+      )
+        .then((blob) => {
+          return blob.json();
+        })
+        .then((response) => {
+          console.log(response);
+          let temperature = response.current.temp;
+          let humidity = response.current.humidity;
+          let windSpeed = response.current.wind_speed;
+          let UVIndex = response.current.uvi;
+          let icon = response.current.weather[0].icon;
+          console.log(
+            "temp: " +
+              temperature +
+              " humidity: " +
+              humidity +
+              " Wind Speed " +
+              windSpeed +
+              " Uv Index " +
+              UVIndex +
+              " icon " +
+              icon
+          );
+        });
     });
 }
 //let search begin on button click
