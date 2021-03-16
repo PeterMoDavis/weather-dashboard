d73705e61c5b8537354b3231f5ea109c87a1f4d0
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-16 18:54:32 -0400

    did way too much js and html before commiting

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,24 +10,42 @@ function getWeather(city) {
       return blob.json();
     })
     .then((response) => {
+      console.log(response);
       //save city to local storage
       let cities = $(JSON.parse(localStorage.getItem("cities"))) || [];
       cities.push(response.name);
 
-      //loop through array and list cities
+      let icon = response.weather[0].icon;
+      var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+      //add city name
+      $(".h3").text(`${response.name} (${moment().format("MM/DD/YYYY")})`);
+      $("#icon-image").attr("src", iconurl);
+      //empties the ul before looping through storage again
       $("ul").empty();
+
+      //loop through array and list cities-------------------
+
       cities.each((i) => {
+        //create list element
         let li = $(`<li>${cities[i]}</li>`);
+        //add bootstrap class
         li.addClass("list-group-item");
+        //declare ul
         let ul = $("ul");
+        //append li to ul
         ul.append(li);
-        console.log(li);
       });
+
+      //set cities back in local storage
       localStorage.setItem("cities", JSON.stringify(cities));
+
+      //declare lat and lon for second api call
       let lat = response.coord.lat;
       let lon = response.coord.lon;
+      //address
       let apiAddress2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=a6d51def84a4c7a5cfb3387c9243b441`;
       console.log(lon, lat);
+      //api call
       fetch(apiAddress2)
         .then((blob) => {
           return blob.json();
@@ -39,6 +57,22 @@ function getWeather(city) {
           let humidity = response.current.humidity;
           let windSpeed = response.current.wind_speed;
           let UVIndex = response.current.uvi;
+
+          console.log(icon);
+
+          //add Temperature to the current block
+          $("#temperature").text(`Temperature: ${temperature} °F`);
+          $("#humidity").text(`Humidity: ${humidity} %`);
+          $("#wind-speed").text(`Wind Speed: ${windSpeed} MPH`);
+          $("#uv-index").text(`UV Index: `);
+          $("#uv-index-span").text(UVIndex);
+          if (UVIndex < 3) {
+            $("#uv-index-span").css("background-color", "green");
+          } else if (UVIndex > 3 && UVIndex < 6) {
+            $("#uv-index-span").css("background-color", "rgb(197, 197, 8)");
+          } else {
+            $("#uv-index-span").css("background-color", "red");
+          }
           //5 Day Forecast
           console.log(
             "temp: " +
