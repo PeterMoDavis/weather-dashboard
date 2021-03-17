fa1f4dbf8fd1910b7ba371009c70f88eb88cda08
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-16 21:28:43 -0400

    i did a whole lot of messy stuff all over. again, waited to long to commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 // acct key : a6d51def84a4c7a5cfb3387c9243b441
 let input = $("input");
 let button = $("button");
+
 //get weather by city
 function getWeather(city) {
   let apiAddress1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=a6d51def84a4c7a5cfb3387c9243b441`;
@@ -16,7 +17,7 @@ function getWeather(city) {
       cities.push(response.name);
 
       let icon = response.weather[0].icon;
-      var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+      let iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
       //add city name
       $(".h3").text(`${response.name} (${moment().format("MM/DD/YYYY")})`);
       $("#icon-image").attr("src", iconurl);
@@ -57,6 +58,8 @@ function getWeather(city) {
           let humidity = response.current.humidity;
           let windSpeed = response.current.wind_speed;
           let UVIndex = response.current.uvi;
+          let cardContainer = $(".forecast-container");
+          cardContainer.empty();
 
           console.log(icon);
 
@@ -74,11 +77,32 @@ function getWeather(city) {
             $("#uv-index-span").css("background-color", "red");
           }
           //5 Day Forecast
+
           let days = response.daily;
           for (let i = 1; i <= 5; i++) {
-            console.log(days[i].temp.day);
-            console.log(days[i].humidity);
-            console.log(days[i].weather[0].icon);
+            let h2 = $("h2");
+            let forecastTemp = days[i].temp.day;
+            let forecastHumidity = days[i].humidity;
+            let forecastIcon = days[i].weather[0].icon;
+            let outterDiv = $("<div></div>");
+            let innerDiv = $("<div></div>");
+            let h5 = $("<h5></h5>").addClass("card-title");
+            let image = $("<img>");
+            let tempParagraph = $("<p></p>");
+            let humidityParagraph = $("<p></p>");
+            let iconurl =
+              "http://openweathermap.org/img/w/" + forecastIcon + ".png";
+
+            h2.css("display", "block");
+            outterDiv.attr("class", "card text-white bg-primary");
+            innerDiv.attr("class", "card-body m-3");
+            outterDiv.append(innerDiv);
+            $(".forecast-container").append(outterDiv);
+            h5.text(moment().add([i], "day").format("MM/DD/YYYY"));
+            image.attr("src", iconurl);
+            tempParagraph.text(`Temp: ${forecastTemp} °F`);
+            humidityParagraph.text(`Humidity: ${forecastHumidity}%`);
+            innerDiv.append(h5, image, tempParagraph, humidityParagraph);
           }
         });
     });
@@ -89,3 +113,5 @@ button.on("click", (e) => {
   let city = input.val();
   getWeather(city);
 });
+
+initSavedCities();
